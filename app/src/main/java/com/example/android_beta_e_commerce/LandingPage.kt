516daa81ed65055d673dcf23f4a3d990112d94f6
Ayanda-Junior <112493894+ajsonamzi@users.noreply.github.com>
2023-06-08 21:55:12 +0200

    Landing page

---
+++ app/src/main/java/com/example/android_beta_e_commerce/LandingPage.kt
@@ -0,0 +1,18 @@
+package com.example.android_beta_e_commerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+
+class LandingPage : AppCompatActivity() {
+    lateinit var btn_order: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+
+        this.findViewById<Button>(R.id.btn_order).setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+        }
+    }
+}
