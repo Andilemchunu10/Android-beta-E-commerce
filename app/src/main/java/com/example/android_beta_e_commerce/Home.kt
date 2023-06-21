168a63e5df85d22ec5b36938d3c99605c8fc2846
Donnedoni Fillies <donnedonifillies@gmail.com>
2023-06-21 11:47:31 +0200

    add to cart

--- app/src/main/java/com/example/android_beta_e_commerce/Home.kt
+++ app/src/main/java/com/example/android_beta_e_commerce/Home.kt
@@ -78,6 +78,7 @@ class Home : AppCompatActivity() {
 
         // Get the cart items from the CartManager
         val cartItems = CartManager.getCartItems()
+
         // Update the cart count TextView
         updateCartCount(cartItems.size)
 
@@ -95,26 +96,9 @@ class Home : AppCompatActivity() {
 
 
     }
-    override fun onResume() {
-        super.onResume()
-
-        // Get the latest cart items from the CartManager
-        val cartItems = CartManager.getCartItems()
-
-        // Update the cart count TextView
-        updateCartCount(CartManager.getCartItems().size)
-
-        // Set cartCount visibility based on the cartItems size
-        cartCount.visibility = if (cartItems.isNotEmpty()) {
-            View.VISIBLE // Display the cartCount TextView
-        } else {
-            View.INVISIBLE // Hide the cartCount TextView
-        }
-    }
 
     private fun updateCartCount(count: Int) {
         cartCount.text = count.toString()
-        cartCount.visibility = if (count > 0) View.VISIBLE else View.INVISIBLE
     }
 
     private fun onCategoryImageClick(category: String) {
